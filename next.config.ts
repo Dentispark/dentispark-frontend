de0ea81bf37c7b58e50b514d9e9e18d6d71b7c53
tobisamcode <samueloluwatobiloba48@gmail.com>
2025-06-15 23:42:15 +0100

    dependencies intallations

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // images: {
+  //   loader: "cloudinary",
+  //   // path: "https://res.cloudinary.com/<your-cloud-name>/image/upload/",
+  //   // You can still whitelist external domains if needed
+  // },
+  webpack(config) {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+
+    return config;
+  },
 };
 
 export default nextConfig;
