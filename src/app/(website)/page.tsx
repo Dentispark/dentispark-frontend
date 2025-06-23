71a55c7211f4f9972063a5a3f53f4922be933eae
tobisamcode <samueloluwatobiloba48@gmail.com>
2025-06-23 18:27:13 +0100

    feat(landing-page): implement api and some other components  of the landing page

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,7 +1,11 @@
+import { Footer } from "@/src/components/layouts/footer";
+import { AdmissionProbabilityCalculator } from "@/src/features/(website)/home/components/admission-probability-calculator";
 import { FreeTools } from "@/src/features/(website)/home/components/free-tools";
 import { Hero } from "@/src/features/(website)/home/components/hero-section";
 import { HowItWorks } from "@/src/features/(website)/home/components/how-it-works";
 import { MeetOurMentors } from "@/src/features/(website)/home/components/meet-our-mentors";
+import { PricingSection } from "@/src/features/(website)/home/components/pricing";
+import { JoinSection } from "@/src/features/(website)/home/components/subscribe";
 import { Testimonials } from "@/src/features/(website)/home/components/testimonials";
 import WhyChooseUs from "@/src/features/(website)/home/components/why-choose-us";
 
@@ -14,6 +18,10 @@ export default function Home() {
       <MeetOurMentors />
       <WhyChooseUs />
       <Testimonials />
+      <AdmissionProbabilityCalculator />
+      <PricingSection />
+      <JoinSection />
+      <Footer />
     </>
   );
 }
