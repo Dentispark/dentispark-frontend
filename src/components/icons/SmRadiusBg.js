7e0d3bbdf4bb3111bbdbca08de7284e10d79b90c
tobisamcode <samueloluwatobiloba48@gmail.com>
2025-06-23 18:26:24 +0100

    feat(landing-page): implement svgs and icons  of the landing page

---
+++ src/components/icons/SmRadiusBg.js
@@ -0,0 +1,3 @@
+import * as React from "react";
+const SvgSmRadiusBg = props => <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 409 409" {...props}><circle cx={204.5} cy={204.5} r={202.5} stroke="#E7F7F1" strokeWidth={4} /></svg>;
+export default SvgSmRadiusBg;
