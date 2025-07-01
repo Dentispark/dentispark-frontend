35985f6ac6dba1642106aa89644e03ca786b195e
tobisamcode <samueloluwatobiloba48@gmail.com>
2025-07-01 11:37:20 +0100

    about-us page implemented

---
+++ src/app/(website)/about-us/page.tsx
@@ -0,0 +1,13 @@
+import { HeroSection } from "@/src/features/(website)/about-us/components/hero-section";
+import { AimSection } from "@/src/features/(website)/about-us/components/aim-section";
+import { TeamSection } from "@/src/features/(website)/about-us/components/team-section";
+
+export default function AboutUs() {
+  return (
+    <>
+      <HeroSection />
+      <AimSection />
+      <TeamSection />
+    </>
+  );
+}
