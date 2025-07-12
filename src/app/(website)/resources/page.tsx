c4ea562fa4e11b1f345a76440901469651c3c2ba
tobisamcode <samueloluwatobiloba48@gmail.com>
2025-07-12 19:48:42 +0100

    feat/resources-page (components)

---
+++ src/app/(website)/resources/page.tsx
@@ -0,0 +1,13 @@
+import {
+  HeroSection,
+  ResourcesGrid,
+} from "@/src/features/(website)/resources/components";
+
+export default function ResourcesPage() {
+  return (
+    <>
+      <HeroSection />
+      <ResourcesGrid />
+    </>
+  );
+}
