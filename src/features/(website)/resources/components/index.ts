da60eb69c6171bfea4b38c40f4b296400aaba11f
tobisamcode <samueloluwatobiloba48@gmail.com>
2025-07-13 00:16:46 +0100

    feat/resources-page (components)

--- src/features/(website)/resources/components/index.ts
+++ src/features/(website)/resources/components/index.ts
@@ -1,2 +1,4 @@
 export { HeroSection } from "./hero-section";
 export { ResourcesGrid } from "./resources-grid";
+export { ResourcesGridSecondary } from "./resources-grid-secondary";
+export { ResourceDetail } from "./resource-detail";
